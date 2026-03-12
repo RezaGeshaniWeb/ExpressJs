5f36f0600cde7d08c3883ea9ad838729d70ba5d0
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-12 14:41:47 +0330

    Regex in Route part 2

--- 01- Routing/matched-routes.js
+++ 01- Routing/matched-routes.js
@@ -21,4 +21,16 @@ app.get('/ab(cd)?e', (req, res) => {
     res.status(200).send("Accepted: " + req.url)
 })
 
+app.get(/a/, (req, res) => {
+    res.status(200).send("Accepted: " + req.url)
+})
+
+app.get(/[a-z0-9\.\_]{5,50}@[a-z]{2,6}\.[a-z]{2,10}/, (req, res) => {
+    res.status(200).send("Accepted: " + req.url)
+})
+
+app.get(/.*nodejs$/, (req, res) => {
+    res.status(200).send("Accepted: " + req.url)
+})
+
 res.listen(3000, () => console.log('server run on port 3000'))
