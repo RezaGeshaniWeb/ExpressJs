0d852e702b5c96196a1ab27300677200fab47af0
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-12 16:07:18 +0330

    search with queryString

--- 01- Routing/query-string.js
+++ 01- Routing/query-string.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const queryString = require('querystring')
+const posts = require('./post.json')
 
-app.get('/posts', (req, res) => {
+app.get('/blogs', (req, res) => {
     // res.send(req.query)
 
     console.log(queryString.parse("key1=value1&key2=value2&key3=value3"))
@@ -15,6 +16,14 @@ app.get('/posts', (req, res) => {
     const { key1, key2, key3 } = req.query;
 
     res.send({ key1, key2, key3, url: req.originalUrl })
+})
+
+app.get('/posts', (req, res) => {
+    const { title, desc } = req.query
+    const regexpTitle = new RegExp(title ?? '', 'gi')
+    const regexpDesc = new RegExp(desc ?? '', 'gi')
+    const filter = posts.filter(p => (p.title.match(regexpTitle) || p.body.match(regexpDesc)))
+    res.send({ posts: filter })
 })
 
 res.listen(3000, () => console.log('server run on port 3000'))
