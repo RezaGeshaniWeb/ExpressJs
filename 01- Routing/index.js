0c9fa7d1bf5516deb1ae962432f7430ad4578a43
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-12 12:45:20 +0330

    Routing part 2

--- 01- Routing/index.js
+++ 01- Routing/index.js
@@ -4,7 +4,18 @@ const app = express()
 app.get('/', (req, res) => {
     // res.send('Hello ExpressJs')
     // res.send('<h1>Hello ExpressJs</h1>')
-    res.send({ message: 'Hello ExpressJs' })
+    res.status(200).send({ message: 'Hello ExpressJs' })
+})
+
+app.get('/users', (req, res) => {
+    res.statusCode = 200
+    res.json({
+        users: [
+            { id: 1, name: 'user1' },
+            { id: 2, name: 'user2' },
+            { id: 3, name: 'user3' }
+        ]
+    })
 })
 
 res.listen(3000, () => console.log('server run on port 3000'))
