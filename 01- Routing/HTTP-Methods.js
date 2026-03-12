4b27cb6af7a838037aa8a16594585421b6766000
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-12 19:04:07 +0330

    middleware part 1

---
+++ 01- Routing/HTTP-Methods.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.post('/body', (req, res) => {
+    res.send(req.body)
+})
+
+app.put('/body', (req, res) => {
+    res.send(req.body)
+})
+
+app.patch('/body', (req, res) => {
+    res.send(req.body)
+})
+
+res.listen(3000, () => console.log('server run on port 3000'))
--- 02- Middleware/index.js
+++ 02- Middleware/index.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded())
-
-app.post('/body', (req, res) => {
-    res.send(req.body)
+app.use((req, res, next) => {
+    console.log('log 1');
+    next()
 })
 
-app.put('/body', (req, res) => {
-    res.send(req.body)
+app.use((req, res, next) => {
+    console.log('log 2');
+    next()
+    
+}, (req, res, next) => {
+    console.log('log 3');
+    next()
 })
 
-app.patch('/body', (req, res) => {
-    res.send(req.body)
+app.get('/', (req, res) => {
+    res.send('main route')
 })
 
 res.listen(3000, () => console.log('server run on port 3000'))
