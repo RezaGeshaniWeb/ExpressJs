2abbbcd58e62fcabf53504f4736b09b1110d127a
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-12 13:09:26 +0330

    Parameter in Routing part 1

--- 01- Routing/index.js
+++ 01- Routing/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+const users = [
+    { id: 1, name: 'user1' },
+    { id: 2, name: 'user2' },
+    { id: 3, name: 'user3' }
+]
+
 app.get('/', (req, res) => {
     // res.send('Hello ExpressJs')
     // res.send('<h1>Hello ExpressJs</h1>')
@@ -9,13 +15,28 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.statusCode = 200
-    res.json({
-        users: [
-            { id: 1, name: 'user1' },
-            { id: 2, name: 'user2' },
-            { id: 3, name: 'user3' }
-        ]
-    })
+    res.json({ users })
+})
+
+app.get('/users/:id', (req, res) => {
+    // const id = req.params.id
+    const { id } = req.params
+    const user = users.find(user => user.id == id)
+    if (!user) {
+        res.status(404).json({
+            statusCode: res.statusCode,
+            error: {
+                message: 'user not found'
+            }
+        })
+    } else {
+        res.status(200).json({
+            statusCode: res.statusCode,
+            data: {
+                user
+            }
+        })
+    }
 })
 
 res.listen(3000, () => console.log('server run on port 3000'))
