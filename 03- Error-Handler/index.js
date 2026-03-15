881b0f1eaf0f2caeb2fe34a573b21d836f73b42b
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-15 19:55:29 +0330

    another error handlers in app

--- 03- Error-Handler/index.js
+++ 03- Error-Handler/index.js
@@ -1,4 +1,3 @@
-// 404
 const express = require('express')
 
 const app = express()
@@ -7,12 +6,23 @@ app.get('/', (req, res) => {
     res.send("index address")
 })
 
+// 404
 app.use((req, res, next) => {
     return res.status(404).json({
         statusCode: res.statusCode,
         error: {
             type: 'NotFound',
             message: `not found ${req.url} route.`
+        }
+    })
+})
+
+// another error
+app.use((err, req, res, next) => {
+    return res.json({
+        statusCode: err.status || 500,
+        error: {
+            message: err.message || 'internalServerError'
         }
     })
 })
