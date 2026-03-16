0fee1d97d5c0c26f040e06726d0f49ba6337cc94
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-16 15:39:57 +0330

    use variable & loops in ejs

--- 04-Template-Engines/ejs/index.js
+++ 04-Template-Engines/ejs/index.js
@@ -4,12 +4,21 @@ const path = require('path')
 
 const app = express()
 
+const links = [
+    { id: 1, text: 'About us' },
+    { id: 2, text: 'F.A.Q' },
+    { id: 3, text: 'Our team' }
+]
+
 app.use("/static", express.static("public"))
 
 app.set("view engine", "ejs")
 
 app.get('/', (req, res, next) => {
-    res.render('index')
+    res.render('index', {
+        links,
+        title: "We strive for excellence in everything we do."
+    })
 })
 
 app.use(NotFoundError)
