add5f643144562bd2093928ee31eea0d270107dd
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-16 14:17:01 +0330

    add partials to hbs

--- 04-Template-Engines/hbs/index.js
+++ 04-Template-Engines/hbs/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const { NotFoundError, ErrorHandler } = require('./util/errorHandler')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 
 app.use(express.static("public"))
 app.set("view engine", "hbs")
 app.set("views", path.join(__dirname, "views"))
+hbs.registerPartials(path.join(__dirname, "views/partials"))
 
 app.get('/', (req, res, next) => {
     res.render('index', {})
