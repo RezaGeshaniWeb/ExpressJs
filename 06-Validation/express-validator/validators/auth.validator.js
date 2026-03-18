9c48d98690d501ee50f958636e4209c998a5aa14
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-18 15:28:19 +0330

    custom validation

--- 06-Validation/express-validator/validators/auth.validator.js
+++ 06-Validation/express-validator/validators/auth.validator.js
@@ -7,10 +7,23 @@ const loginValidator = () => [
 
 const registerValidator = () => [
     body('fullName').isLength({ min: 5, max: 35 }).withMessage('fullname is required, please enter your fullname'),
-    body('age').isNumeric().withMessage('age must to be number'),
+    body('age').custom((value) => {
+        if (isNaN(value)) {
+            throw new Error('age must to be number')
+        } else if (+value > 90 || +value < 12) {
+            throw new Error('your age is invalid age for register')
+        }
+        return true
+    }),
     body('mobile').isMobileumber(['fa-IR', 'en-US']).withMessage('mobile format is invalid'),
     body('email').isEmail().withMessage('email is invalid'),
     body('password').isLength({ min: 6, max: 20 }).withMessage('password is invalid'),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("password does not match with confirmation")
+        }
+        return true
+    })
 ]
 
 module.exports = { loginValidator, registerValidator }
