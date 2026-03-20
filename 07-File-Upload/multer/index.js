3f77708b71a4f10039ada8ddc72c4b695c479aae
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-20 13:09:32 +0330

    upload file in multer

--- 07-File-Upload/multer/index.js
+++ 07-File-Upload/multer/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const multer = require('multer')
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 
-app.post('/upload', (req, res) => { 
-    res.send(req.body)
+const uploadFile = multer({ dest: "uploads/" })
+
+app.post('/upload', uploadFile.single('image'), (req, res) => {
+    res.send(req.file)
 })
 
 app.listen(3000, console.log('server run on port 3000'))
