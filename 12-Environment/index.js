288b06abbb375337abcea91050dc125fb2d336e2
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-22 01:28:17 +0430

    dev & prod environment variables

--- 12-Environment/index.js
+++ 12-Environment/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv')
 
 const app = express()
 
 dotenv.config()
+const NodeEnv = process.env.NODE_ENV
+dotenv.config({
+    path: path.join(__dirname, `.${NodeEnv}.env`)
+})
 
 app.get('/', (req, res) => {
     console.log(process.env.API_KEY)
