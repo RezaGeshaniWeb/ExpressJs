ec8125f31286c1223fc09e085e8bd494ce4ef4af
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-22 02:00:04 +0430

    create hashPassword & verify password

---
+++ 13-Hashing/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hashing')
+})
+
+app.listen(3000, () => console.log('server run on port 3000'))
