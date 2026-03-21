04aa7d97b9512d94cefb5ce52c21a679a9f0d2bf
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-21 20:36:37 +0330

    get cookies

--- 11-Cookie-Parser/index.js
+++ 11-Cookie-Parser/index.js
@@ -7,7 +7,13 @@ app.use(cookieParser())
 
 app.get('/set-cookie', (req, res) => {
     res.cookie('cookieName', 'cookieValue')
+    res.cookie('nodejs', 'cookies')
     res.send('cookie have been saved successfully')
+})
+
+app.get('/get-cookie', (req, res) => {
+    const cookies = req.cookies
+    res.send(cookies)
 })
 
 app.listen(3000, () => console.log('server run on port 3000'))
