6fba05e3e3de120b0c2d3eafb1eea4c6d69685b4
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-22 20:11:59 +0430

    createHash method

--- 13-Hashing/crypto.js
+++ 13-Hashing/crypto.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto')
 
-function hashPassword() {
+function hashPassword(password) {
     const salt = crypto.randomBytes(16).toString('hex')
-    const hash = crypto.pbkdf2Sync("123456", salt, 1000, 64, "sha512").toString('hex')
+    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString('hex')
     const newHash = `$2s.${salt}.${hash}`
-    return newHash    
+    return newHash
 }
 
 function verifyHashPassword(password, hashPassword) {
