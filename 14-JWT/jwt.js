183c174e2311547fa87399d82993610debd487e7
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-23 02:08:54 +0430

    verify & decode jwt

--- 14-JWT/jwt.js
+++ 14-JWT/jwt.js
@@ -1,12 +1,13 @@
 const jwt = require("jsonwebtoken")
-const fs = require("fs")
 
-const privateKey = fs.readFileSync("privateKey.key")
 const secret = "6f9baf3cd6e8b8a73c2cdced37fe9f59226e27d"
 
-const token = jwt.sign({ id: "some_hash_id", email:"me@gmail.com" }, privateKey, {
-    expiresIn: "1w",    // 1s-1d-50d-1y
-    algorithm: "RS512",
+const token = jwt.sign({ id: "some_hash_id", email:"me@gmail.com" }, secret, {
+    expiresIn: "1d",
 })
 
-console.log(token)
+const verifiedData = jwt.verify(token, secret)
+console.log(verifiedData)
+
+const decodeData = jwt.decode(token)
+console.log(decodeData)
