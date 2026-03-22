707aa10f257e6a869c0001e360e1aafc92a96379
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-23 01:36:39 +0430

    create token with jwt type & rs512 algorithm

--- 14-JWT/jwt.js
+++ 14-JWT/jwt.js
@@ -1,10 +1,12 @@
 const jwt = require("jsonwebtoken")
+const fs = require("fs")
 
+const privateKey = fs.readFileSync("privateKey.key")
 const secret = "6f9baf3cd6e8b8a73c2cdced37fe9f59226e27d"
 
-const token = jwt.sign({ id: "some_hash_id", email:"me@gmail.com" }, secret, {
+const token = jwt.sign({ id: "some_hash_id", email:"me@gmail.com" }, privateKey, {
     expiresIn: "1w",    // 1s-1d-50d-1y
-    algorithm: "HS512",
+    algorithm: "RS512",
 })
 
 console.log(token)
