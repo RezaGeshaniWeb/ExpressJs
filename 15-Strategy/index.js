5c66a3fb3b46b2322b0517123e2811750bfeb67f
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-24 01:56:33 +0430

    another strategies

--- 15-Strategy/index.js
+++ 15-Strategy/index.js
@@ -7,3 +7,16 @@ const decodedData = Buffer.from(base64Data, "base64").toString("ascii")
 const [username, password] = decodedData.split("/")
 console.log(username, password)
 
+
+// bearer
+// req(login) => (verify) res => token jwt(xxx.yyy.zzz) => client
+// => headers: {authorization: Bearer Token}
+
+
+// API-KEY
+// queryString => https:domain.com?api-key=token
+// headers: {X-APY-KEY:token}
+
+// Digest
+
+// OAuth
