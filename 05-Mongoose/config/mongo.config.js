06f443fa4fb214189fff566f7fb34442fa4b2ce8
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-03-30 23:26:15 +0430

    fix mongodb

--- 05-Mongoose/config/mongo.config.js
+++ 05-Mongoose/config/mongo.config.js
@@ -3,4 +3,9 @@ const { default: mongoose } = require('mongoose')
 const DB_URL = 'mongodb://localhost:27017/mongoose'
 
 mongoose.set("strictQuery", true)
-mongoose.connect(DB_URL, err => console.log(err ? err.message : 'server connected to mongodb'))
+mongoose.connect(DB_URL)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => {
+        console.error(err.message);
+        process.exit(1);
+    });
