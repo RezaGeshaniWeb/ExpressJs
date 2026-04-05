10cf7e48fa1406d8da81ea0e575a6134dc52ac48
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-04-05 10:57:19 +0430

    fix Hashing & JWT

--- 13-Hashing/crypto.js
+++ 13-Hashing/crypto.js
@@ -2,15 +2,21 @@ const crypto = require('crypto')
 
 function hashPassword(password) {
     const salt = crypto.randomBytes(16).toString('hex')
+    console.log(`salt hashPassword: ${salt}`)
     const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString('hex')
+    console.log(`hash hashPassword: ${hash}`)
     const newHash = `$2s.${salt}.${hash}`
+    console.log(`newHash hashPassword: ${newHash}`)
     return newHash
 }
 
 function verifyHashPassword(password, hashPassword) {
     const salt = hashPassword.split('.')?.[1]
+    console.log(`salt verifyHashPassword: ${salt}`)
     const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString('hex')
+    console.log(`hash verifyHashPassword: ${hash}`)
     const newHash = `$2s.${salt}.${hash}`
+    console.log(`newHash verifyHashPassword: ${newHash}`)
     return (newHash == hashPassword)
 }
 
