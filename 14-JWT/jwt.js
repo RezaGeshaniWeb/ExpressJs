09224e3ab637f6eda2eccb5b0888adf76c625057
RezaGeshaniWeb <rezageshaniweb@gmail.com>
2026-04-08 23:24:14 +0430

    fix jwt

--- 14-JWT/jwt.js
+++ 14-JWT/jwt.js
@@ -5,6 +5,7 @@ const secret = "6f9baf3cd6e8b8a73c2cdced37fe9f59226e27d"
 const token = jwt.sign({ id: "some_hash_id", email:"me@gmail.com" }, secret, {
     expiresIn: "1d",
 })
+console.log(token)
 
 const verifiedData = jwt.verify(token, secret)
 console.log(verifiedData)
